036bfa646970534da951a3c15fd49c61f19e6a39
Christie Hua <christiehua@gmail.com>
2014-06-18 19:03:16 -0700

    twitter controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140619011324) do
+ActiveRecord::Schema.define(version: 20140619020038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,18 @@ ActiveRecord::Schema.define(version: 20140619011324) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+  end
+
+  create_table "tweets", force: true do |t|
+    t.string   "tweet_id"
+    t.string   "user_name"
+    t.string   "text"
+    t.string   "latitude"
+    t.string   "longitude"
+    t.string   "sentiment"
+    t.datetime "created_at"
+    t.string   "timestamps"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
