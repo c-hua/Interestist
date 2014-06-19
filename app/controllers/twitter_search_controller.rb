fc99d43d3c8621579e574621a3f907c1dfc2aeeb
Christie Hua <christiehua@gmail.com>
2014-06-18 19:16:40 -0700

    twitter controller

--- app/controllers/twitter_search_controller.rb
+++ app/controllers/twitter_search_controller.rb
@@ -1,16 +1,45 @@
+class TwitterSearchController < ApplicationController
+
 require 'json'
 require 'rubygems'
 require 'twitter'
+require 'tweetstream'
+respond_to :json, :html
 
-class TwitterSearchController < ApplicationController
   def index
   	# json = File.read(Rails.root + "app/assets/tweets.json")
   	# @tweets = JSON.parse(json)
-  	@tweets = Tweet.all.where
+  	@tweets = Tweet.new
+
+  	if params[:search_id]
+  		@s = Tweets.find(params[:search_id])
+  		get_tweets(@s)
+  	end
+
+  	@tweets = Tweet.all
+  	respond_to do |format|
+  		format.html
+  		format.json do
+  			render json: @tweets
+  		end
+  	end
+  end
+
+  def new
+  	@tweets = Tweet.new
+  end
+
+  def create
+  	@tweets = Tweet.new(search_params)
+  	@tweets.user = current_user
+  end
+
+  def show
+  	result = @response.body
   end
 
   def reload
-  	redirect_to :root
+  	result = @response.body
   end
 
 end
