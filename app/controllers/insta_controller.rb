1f9f17c792c457be349ac58e0ff006265d011d60
Christie Hua <christiehua@gmail.com>
2014-06-23 15:06:19 -0700

    updated root view

--- app/controllers/insta_controller.rb
+++ app/controllers/insta_controller.rb
@@ -1,11 +1,11 @@
-class InstaController < ApplicationController
-    def index
-        @instagram = Instagram.media_popular({:count => 2})
-        @instagramga = Instagram.media_search("34.012836", "-118.495338", {:count => 4})
-        #@instagramla = Instagram.location_search("34.012836", "-118.495338", {:count => 1})
+# class InstaController < ApplicationController
+#     def index
+#         @instagram = Instagram.media_popular({:count => 2})
+#         @instagramga = Instagram.media_search("34.012836", "-118.495338", {:count => 4})
+#         #@instagramla = Instagram.location_search("34.012836", "-118.495338", {:count => 1})
        
  
-    @tag = Instagram.tag_recent_media( "<%= garticle %>", {:count => 2})
-        end
-    end
+#     @tag = Instagram.tag_recent_media( "<%= garticle %>", {:count => 2})
+#         end
+#     end
 
