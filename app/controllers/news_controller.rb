422e0ea10dc83d0893b28806de46f89197d3651e
zekeslotsky <zekejacobs@hotmail.com>
2014-06-23 15:47:00 -0700

    usatoday&insta on 1 page

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -7,9 +7,17 @@ class NewsController < ApplicationController
 
   	# using Instagram API
 
+        # @instagram = Instagram.media_popular({:count => 2})
+        # @instagramga = Instagram.media_search("34.012836", "-118.495338", {:count => 4})
+        # #@instagramla = Instagram.location_search("34.012836", "-118.495338", {:count => 1})
+       
+ 
+    @tag = Instagram.tag_recent_media( "#{@keyword}", {:count => 4})
+        end
+
 
   	# using Twitter API
 
 
-  end
-end
+ end
+
