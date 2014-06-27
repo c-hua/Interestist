9657ca27aa19c95d5e71e6b85cc6a611a02e5970
Christie Hua <christiehua@gmail.com>
2014-06-27 12:12:44 -0700

    sessions controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,18 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe SessionsController, :type => :controller do
+describe SessionsController do
+  let :valid_attributes do
+    {
+      :user_id => @user.id
+    }
+  end
+  
 
   describe "GET 'create'" do
     it "returns http success" do
       get 'create'
       expect(response).to be_success
     end
+
+      it "should create a new user" do
+        expect do
+          post :create, :user => user_params
+        end.to change(User, :count).by(1)
+      end
   end
 
   describe "GET 'destroy'" do
-    it "returns http success" do
+    before do
+      @user = User.create! valid_attributes
+    end
+
+    it "returns destroy success" do
       get 'destroy'
-      expect(response).to be_success
+      expect(response).to be_redirect_to root_path
     end
   end
 
