836abc0159b429865ce28ad132e4f76fef651c05
Christie Hua <christiehua@gmail.com>
2014-06-27 12:13:40 -0700

    twitter search controller specs

--- spec/controllers/twitter_search_controller_spec.rb
+++ spec/controllers/twitter_search_controller_spec.rb
@@ -1,7 +1,17 @@
 require "rails_helper"
 
 describe TwitterSearchController do
+    let :valid_search do
+    {
+      term: "soccer"
+    }
+  end
   describe "GET #index" do
+    before do 
+      @search1 = Search.create! valid_search
+      get :index
+    end
+
     it "responds successfully with an HTTP 200 status code" do
       get :index
       expect(response).to be_success
