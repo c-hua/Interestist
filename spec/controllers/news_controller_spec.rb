a37ec1340d15987b0a1d7a2c3b422420a13e1542
Christie Hua <christiehua@gmail.com>
2014-06-27 12:12:04 -0700

    news controller specs

--- spec/controllers/news_controller_spec.rb
+++ spec/controllers/news_controller_spec.rb
@@ -1,12 +1,42 @@
 require 'rails_helper'
 
-RSpec.describe NewsController, :type => :controller do
+describe NewsController do
+		let :valid_keyword do
+		{
+			keyword: "usa"
+		}
+	end
 
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
+  describe "GET #index" do
+
+		before do
+			@keyword = params[:keyword]
+			get :index
+		end
+
+		before do
+     	@searchusa1 = Page.party valid_keyword
+			get :index
+		end
+
+		before do
+     @tag1 = Instagram.tag_recent_media valid_keyword
+			get :index
+		end
+
+		before do
+			@s = params[:keyword]
+   		get :index
+		end
+
+		it "should render the index template" do
+			expect(response).to render_template :index
+		end
+
+    it "responds successfully with an HTTP 200 status code" do
+      get :index
       expect(response).to be_success
+      expect(response.status).to eq(200)
     end
   end
-
 end
